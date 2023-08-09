0236aa83db2449ad8600f336dc526195052212cb
Aleksei Smirnov <sibusky@yandex.ru>
2023-08-09 22:13:29 +0300

    feat: install dependencies: eslit, dotenv; add scripts to start, develop and linting

---
+++ app.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+const { NODE_ENV, DB_CONNECTION_STRING, PORT } = process.env;
+
+
+console.log(PORT)
--- test.js
+++
@@ -1,4 +0,0 @@
-const sum = 1+1;
-
-console.log(sum)
-console.log('hello')
