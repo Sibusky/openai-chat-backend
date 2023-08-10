194cbc4b9a18632f42b12b371b396559e1b5fcbe
Aleksei Smirnov <sibusky@yandex.ru>
2023-08-10 23:26:48 +0300

    feat: add CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const { DB_URL, PORT } = process.env;
 
@@ -10,6 +11,14 @@ const app = express();
 // Convert request body to JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Allow CORS
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  }),
+);
 
 // Routes import
 app.use(require('./routes/index'));
