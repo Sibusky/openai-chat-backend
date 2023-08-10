6f4b93715b7a4e9795901fd4fd21b4b7580d5eec
Aleksei Smirnov <sibusky@yandex.ru>
2023-08-10 18:13:26 +0300

    feat: add auth middleware

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.use(require('./start'));
-// router.use(require('./chat'));
+router.use(require('./chat'));
 
 router.use('/*', (req, res, next) => next(res.status(404).json('Page not found')));
 
