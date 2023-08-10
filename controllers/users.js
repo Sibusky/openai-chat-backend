376fbd3ca92c5603f467ff2839cf97877a6ce115
Aleksei Smirnov <sibusky@yandex.ru>
2023-08-10 18:06:50 +0300

    feat: add controllers for login and registration

---
+++ controllers/users.js
@@ -0,0 +1,53 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/users');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+// Login
+module.exports.login = (req, res, next) => {
+  const { loginName, loginPassword } = req.body;
+  return User.findUserByCredentials(loginName, loginPassword)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {
+        expiresIn: '7d',
+      });
+      res.send({ token });
+    })
+    .catch(() => {
+      const unauthorizedError = new Error('Authentication failed, incorrect name or password');
+      unauthorizedError.status = 401;
+      res.send(unauthorizedError.message);
+      next(unauthorizedError);
+    });
+};
+
+// Registration
+module.exports.createUser = (req, res, next) => {
+  const { name, password } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User.create({
+      password: hash,
+      name,
+    }))
+    .then((user) => res.status(201).send({
+      name: user.name,
+      _id: user._id,
+    }))
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        const badRequestError = new Error('Validation Error. Check input data for name and password');
+        badRequestError.status = 400;
+        res.send(badRequestError.message);
+        next(badRequestError);
+      } else if (error.code === 11000) {
+        const conflictError = new Error('Conflict Error. User with this name already exists');
+        conflictError.status = 1100;
+        res.send(conflictError.message);
+        next(conflictError);
+      } else {
+        next(error);
+      }
+    });
+};
--- routes/start.js
+++ routes/start.js
@@ -1,12 +1,6 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-// const { login } = require('../controllers/users');
-// const { createUser } = require('../controllers/users');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/users');
-
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { login, createUser } = require('../controllers/users');
 
 // Login validation using celebrate
 const loginValidation = celebrate({
@@ -24,48 +18,10 @@ const bodyValidation = celebrate({
   }),
 });
 
-// Login route
-router.post('/start', loginValidation, (req, res, next) => {
-  const { loginName, loginPassword } = req.body;
+// Login
+router.post('/start', loginValidation, login);
 
-  return User.findUserByCredentials(loginName, loginPassword)
-    .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {
-        expiresIn: '7d',
-      });
-      res.send({ token });
-    })
-    .catch((error) => {
-      next(res.status(401).json(error));
-    });
-});
-
-// Registration route
-router.post('/start', bodyValidation, (req, res, next) => {
-  const { name, password } = req.body;
-  bcrypt
-    .hash(password, 10)
-    .then((hash) =>
-      User.create({
-        password: hash,
-        name,
-      })
-    )
-    .then((user) =>
-      res.status(201).send({
-        name: user.name,
-        _id: user._id,
-      })
-    )
-    .catch((error) => {
-      if (error.name === 'ValidationError') {
-        next(res.status(400).json(error));
-      } else if (error.code === 11000) {
-        next(res.status(1100).json(error));
-      } else {
-        next(error);
-      }
-    });
-});
+// Registration
+router.post('/start', bodyValidation, createUser);
 
 module.exports = router;
